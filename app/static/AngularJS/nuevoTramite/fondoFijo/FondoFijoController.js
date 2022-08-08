ec55e2e60201d295b9d1f8859f815b713b941d68
Eyebrah07 <eduardo.yebra@coalmx.com>
2022-08-08 17:23:25 -0500

    cambiosDatosFechasMensajes

--- app/static/AngularJS/nuevoTramite/fondoFijo/FondoFijoController.js
+++ app/static/AngularJS/nuevoTramite/fondoFijo/FondoFijoController.js
@@ -1873,17 +1873,19 @@ $scope.verPdfComprobacion = function(item) {
 
                         // POLIZA COMPROBACION DE MENOS REGRESA EFECTIVO
                         // tipoProceso = await promiseInsertaDatos($scope.idUsuario, $scope.sucursalVale , 10,$scope.nombreVale , monto, '', $scope.nombreDepartamentoVale, JSON.parse(localStorage.getItem('borrador')).idPerTra, '', CCDepto);
-                        //$scope.dataComplementoFF();                       
-                        $scope.montoCVFM = monto
-                        $scope.insertaPolizaFFCVFM();
-
-                        fondoFijoRepository.valeSinComprobar($scope.idVale,monto).then(function (result) {
-                            if (result.data.length > 0) {
-                                $scope.listaValesFF(JSON.parse(localStorage.getItem('borrador')).idPerTra, $scope.idVale);
-                                $scope.regresarVale();
-                                $('#loading').modal('hide');
-                            }
-                        }); 
+                        //$scope.dataComplementoFF();   
+                        if (estatusVales.monto > 0) {
+                            $scope.montoCVFM = estatusVales.monto 
+                            $scope.personaCVFM = estatusVales.personaCVFM
+                            $scope.insertaPolizaFFCVFM();
+                        }    
+                        // fondoFijoRepository.valeSinComprobar($scope.idVale,monto).then(function (result) {
+                        //     if (result.data.length > 0) {
+                        //         $scope.listaValesFF(JSON.parse(localStorage.getItem('borrador')).idPerTra, $scope.idVale);
+                        //         $scope.regresarVale();
+                        //         $('#loading').modal('hide');
+                        //     }
+                        // }); 
             
                 
                     $('#loading').modal('hide');
@@ -3789,7 +3791,7 @@ $scope.insertaPolizaFrontAPIGastosInventario = async function () {
 
 
 
-    if($scope.datoPoliza.justificoMas == 1)
+    if($scope.datoPoliza.justificoMas == 1 && $scope.datoPoliza.montoCVFR > 0)
         {
             $scope.insertaPolizaFrontCVFRInventario()
         }
